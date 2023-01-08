280b56dac683390dbbd4a5e24c2a400d6fedac80
Sameekshshetty <sameeksh.ee17@bmsce.ac.in>
2023-01-08 11:28:03 +0530

    New file created

---
+++ test.c
@@ -0,0 +1,25 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  test.c
+ *
+ *    Description: 
+ *
+ *
+ *        Version:  1.0
+ *        Created:  08/01/23 11:22:27 AM IST
+ *       Revision:  none
+ *       Compiler:  gcc
+ *
+ *         Author:  Sameeksh M Shetty (sam), sameekshshetty1227@gmail.com
+ *        Company:  L L L L L L L L L L |COMPANY| T T T T T T T T T T
+ *
+ * =====================================================================================
+ */
+
+#include <stdio.h>
+
+int main(int argc,char** argv){
+printf("Hello world\n");
+return 0;
+    }
